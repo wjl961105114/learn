e83166ab39a65188932cbf87f93d8e04f21dc1b2
allen00000 <961105114@qq.com>
2019-01-14 14:11:12 +0800

    Initial commit

---
+++ src/main/java/com/wjl/LearnApplication.java
@@ -0,0 +1,20 @@
+package com.wjl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author wangjialun
+ * @date 2019/1/14
+ */
+@SpringBootApplication
+public class LearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnApplication.class, args);
+	}
+
+}
+
